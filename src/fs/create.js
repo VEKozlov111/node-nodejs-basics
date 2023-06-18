808e6f7b30394b52102bcafce3cc9e0ddef81fd8
VEKozlov111 <vlad.kozlov.2015@gmail.com>
2023-06-18 13:32:48 +0300

    feat: add create func

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,25 @@
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+
 const create = async () => {
-    // Write your code here 
+  /*fs.writeFile('./fs/files/fresh.txt', 'I am fresh and young', (error) => {
+    if (error) {
+      console.log(error)
+    }
+  })*/
+  const dir = path.dirname(fileURLToPath(import.meta.url));
+  const file = path.resolve(dir, 'files', 'fresh.txt');
+  fs.access(file, (error) => {
+    if (error) {
+      fs.appendFile(file, 'I am fresh and young', (error) => {
+        if (error) throw error;
+      });
+      return;
+    }
+    throw new Error('FS operation failed');
+  });
 };
 
 await create();
