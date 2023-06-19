f5c20da17c0b9db99bb304e5b40093fe1673089c
VEKozlov111 <vlad.kozlov.2015@gmail.com>
2023-06-19 23:58:57 +0300

    feat: add calcHach func

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import fs from 'fs/promises';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import { createHash } from 'crypto';
+
+
 const calculateHash = async () => {
-    // Write your code here 
+  const dir = path.dirname(fileURLToPath(import.meta.url));
+  const file = path.resolve(dir, 'files', 'fileToCalculateHashFor.txt');
+  const content = fs.readFile(file);
+
+  content
+    .then((content) => {
+      const hash = createHash('sha256').update('' + content).digest('hex');
+      console.log(hash)
+    })
+
 };
 
 await calculateHash();
