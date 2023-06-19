b945ab18aca93759bc50f7b52467feeb24f49e9a
VEKozlov111 <vlad.kozlov.2015@gmail.com>
2023-06-19 21:58:29 +0300

    feat: add read func

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,23 @@
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+
 const read = async () => {
-    // Write your code here 
+  const dir = path.dirname(fileURLToPath(import.meta.url));
+
+  const file = path.resolve(dir, 'files', 'fileToRead.txt');
+  const checkFile = fs.promises.access(file);
+
+  checkFile.then(
+    result => {
+      fs.promises.readFile(file, { encoding: 'utf8' })
+        .then((text) => console.log(text))
+    },
+    error => { throw new Error('FS operation failed'); }
+
+
+  )
 };
 
 await read();
