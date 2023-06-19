f197a6ea89a705b063ec57632f32b67bc54d34c4
VEKozlov111 <vlad.kozlov.2015@gmail.com>
2023-06-19 21:06:21 +0300

    feat: add copy func

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,37 @@
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 const copy = async () => {
-    // Write your code here 
+  const dir = path.dirname(fileURLToPath(import.meta.url));
+
+  const folderFiles = path.resolve(dir, 'files');
+  const folderFilesNew = path.resolve(dir, 'files_copy');
+
+  const checkFolderFiles = fs.promises.access(folderFiles);
+  const checkFolderFilesNew = fs.promises.access(folderFilesNew);
+
+  const checkErr = Promise.allSettled([checkFolderFiles, checkFolderFilesNew])
+
+  checkErr
+    .then((errors) => {
+      if (errors[0].status !== 'fulfilled' || errors[1].status !== 'rejected') {
+        throw new Error('FS operation failed');
+      }
+    })
+    .then(() => fs.promises.mkdir(folderFilesNew))
+    .then(() => {
+      fs.promises.readdir(folderFiles)
+        .then((files) => {
+          for (let filename of files) {
+            fs.promises.copyFile(
+              path.resolve(folderFiles, filename),
+              path.resolve(folderFilesNew, filename)
+            );
+          }
+
+        })
+    })
 };
 
 await copy();
