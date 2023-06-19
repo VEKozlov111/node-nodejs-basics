bbf9652e866309f7d1d5f9459fb7df8d6c300b0d
VEKozlov111 <vlad.kozlov.2015@gmail.com>
2023-06-19 21:23:45 +0300

    feat: add rename func

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,28 @@
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 const rename = async () => {
-    // Write your code here 
+  const dir = path.dirname(fileURLToPath(import.meta.url));
+
+
+  const file = path.resolve(dir, 'files', 'wrongFilename.txt');
+  const renamedFile = path.resolve(dir, 'files', 'properFilename.md');
+
+  const checkFile = fs.promises.access(file);
+  const checkRenamedFile = fs.promises.access(renamedFile);
+
+
+  const checkErr = Promise.allSettled([checkFile, checkRenamedFile])
+
+  checkErr
+    .then((errors) => {
+      if (errors[0].status !== 'fulfilled' || errors[1].status !== 'rejected') {
+        throw new Error('FS operation failed');
+      }
+    })
+    .then(() => fs.promises.rename(file, renamedFile))
+
 };
 
 await rename();
